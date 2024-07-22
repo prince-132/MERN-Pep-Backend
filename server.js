1307c332a73056ca8698977007894cb4eb00cf4a
Gyanendra Nath Shukla <117109800+prince-132@users.noreply.github.com>
2024-07-22 13:50:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const cookieparser = require('cookie-parser');
-const app = express()
+const cookieParser = require('cookie-parser');
+const app = express();
 const fileUpload = require('express-fileupload');
 const router = require('./routes/router');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cloudinary = require('cloudinary');
 
-dotenv.config()
+dotenv.config();
 
 cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  });
-
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 app.use(cors({
-    origin: ["https://mern-pep-frontend.vercel.app/"],
-    credentials : true
-}))
-app.use(cookieparser())
-
+  origin: ['http://localhost:5173', 'https://mern-pep-frontend.vercel.app'], // Add your frontend domains here
+  credentials: true
+}));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: '/tmp/'
 }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
 mongoose.connect(process.env.MONGO_URI, {
-    dbName: "Assignment"
-}).then(()=>{
-    console.log("DB Connected...");
+  dbName: "Assignment"
+}).then(() => {
+  console.log("DB Connected...");
 });
 
-app.use('/auth', router)
+app.use('/auth', router);
 
-app.listen(3000, ()=>{
-    console.log("Server is listening...");
-})
+app.listen(3000, () => {
+  console.log("Server is listening...");
+});
